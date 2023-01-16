032ce5d0d951874733b9b200e1fbd45900c18548
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-01-16 11:17:01 -0800

    Test: edit merchant bulk discount

--- spec/features/bulk_discounts/show_spec.rb
+++ spec/features/bulk_discounts/show_spec.rb
@@ -1,11 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe 'merchants bulk discount show page' do
-  # it '4: Merchant Bulk Discount Show
-
-  # As a merchant
-  # When I visit my bulk discount show page
-  # Then I see the bulk discount's quantity threshold and percentage discount'
   it 'lists the discount quantity threshold and percentage discount' do
     @merchant1 = Merchant.create!(name: 'Hair Care')
     bd1 = @merchant1.bulk_discounts.create!(percentage: 10, quantity_threshold: 10)
@@ -16,4 +11,33 @@ RSpec.describe 'merchants bulk discount show page' do
     expect(page).to have_content(bd1.quantity_threshold)
     expect(page).to have_no_content(bd2.quantity_threshold)
   end
+
+#   5: Merchant Bulk Discount Edit
+
+# As a merchant
+# When I visit my bulk discount show page
+# Then I see a link to edit the bulk discount
+# When I click this link
+# Then I am taken to a new page with a form to edit the discount
+# And I see that the discounts current attributes are pre-poluated in the form
+# When I change any/all of the information and click submit
+# Then I am redirected to the bulk discount's show page
+# And I see that the discount's attributes have been updated
+  it 'edits merchant bulk discount object' do
+    @merchant1 = Merchant.create!(name: 'Hair Care')
+    bd1 = @merchant1.bulk_discounts.create!(percentage: 10, quantity_threshold: 10)
+
+    
+    visit merchant_bulk_discount_path(@merchant1, bd1)
+    click_link "Edit"
+    expect(current_path).to eq(edit_merchant_bulk_discount_path(@merchant1, bd1))
+    expect(page).to have_no_content(15)
+
+    fill_in 'Percentage', with: 15
+    click_button 'Submit'
+
+    expect(current_path).to eq(merchant_bulk_discount_path(@merchant1, bd1))
+    expect(page).to have_content(15)
+  end
+
 end
